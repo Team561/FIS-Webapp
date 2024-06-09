b9c9e4a3347e995a7b07ff657cc897d3ee26c440
Ruva <ruvarashemakuvise@gmail.com>
2024-06-09 19:06:05 +0200

    Updated map functionality and added markers

--- Webapp/js/script.js
+++ Webapp/js/script.js
@@ -11,4 +11,23 @@ document.addEventListener("DOMContentLoaded", function() {
     L.marker([45.8150, 15.9819]).addTo(map)
         .bindPopup('Zagreb, Croatia<br> Capital City.')
         .openPopup();
+
+    // Define locations where you want to add additional markers
+    var locations = [
+        { lat: 45.812, lng: 15.975, popup: "Marker 1: Location 1" },
+        { lat: 45.820, lng: 15.990, popup: "Marker 2: Location 2" },
+        { lat: 45.817, lng: 15.985, popup: "Marker 3: Location 3" }
+    ];
+
+    // Loop through the locations and add a marker for each one
+    locations.forEach(function(location) {
+        L.marker([location.lat, location.lng])
+            .addTo(map)
+            .bindPopup(location.popup);
+    });
+
+    // Example function to show a notification when a button is clicked
+    document.getElementById('showNotificationBtn').addEventListener('click', function() {
+        alert('Notification: This is an example notification.');
+    });
 });
